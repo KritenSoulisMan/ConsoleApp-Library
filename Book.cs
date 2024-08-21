dd263e2811135d156fdca5953c75daa10edc95ac
Roman <142747461+KritenSoulisMan@users.noreply.github.com>
2024-08-21 19:37:54 +0300

    Library

---
+++ Book.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Library
+{
+    class Book
+    {
+        public string Title;
+        public string Author;
+        public string Description;
+        public int Pages;
+        public string Genre;
+
+
+        public Book(string Title, string Author, string Description, int Pages, string Genre)
+        {
+            this.Title = Title;
+            this.Author = Author;
+            this.Description = Description;
+            this.Pages = Pages;
+            this.Genre = Genre;
+    }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library(); // Для создания класса
+            while (true)
+            {
+                Console.WriteLine("Выберите пункт: ");
+                int arc = int.Parse(Console.ReadLine());
+
+                switch (arc)
+                {
+                    case 0:
+                        library.AddBook();
+                        break;
+                    case 1:
+                        library.ShowBook();
+                        break;
+                }
+            }
+        }
+    }
+}
